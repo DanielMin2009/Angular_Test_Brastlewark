8d4517fe71f7557240a68a9cb37eab9a88e85421
DanielMin2009 <daniel.minguella.blasco@gmail.com>
2020-07-22 10:52:57 +0200

    project arch folders

---
+++ brastlewark/src/app/Views/gnome-detail/gnome-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gnome-detail',
+  templateUrl: './gnome-detail.component.html',
+  styleUrls: ['./gnome-detail.component.scss']
+})
+export class GnomeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- brastlewark/src/app/app.module.ts
+++ brastlewark/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/Shared/navbar/navbar.component';
+import { GnomesListComponent } from './Views/gnomes-list/gnomes-list.component';
+import { GnomeDetailComponent } from './Views/gnome-detail/gnome-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GnomesListComponent,
+    GnomeDetailComponent
   ],
   imports: [
     BrowserModule,
