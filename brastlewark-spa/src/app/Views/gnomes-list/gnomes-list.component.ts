b00c9e1bb11916f296db568d538617200d114df0
DanielMin2009 <daniel.minguella.blasco@gmail.com>
2020-07-23 17:51:29 +0200

    navbar to filter data from JSON

--- brastlewark-spa/src/app/Views/gnomes-list/gnomes-list.component.ts
+++ brastlewark-spa/src/app/Views/gnomes-list/gnomes-list.component.ts
@@ -10,6 +10,8 @@ export class GnomesListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    
   }
 
 }
--- brastlewark-spa/src/app/app-routing.module.ts
+++ brastlewark-spa/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GnomesListComponent } from './Views/gnomes-list/gnomes-list.component';
+import { GnomeDetailComponent } from './Views/gnome-detail/gnome-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "gnomes-list", component: GnomesListComponent },
+  { path: "gnome-detail", component: GnomeDetailComponent },
+  { path: "**", pathMatch: 'full', redirectTo: 'gnomes-list' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
